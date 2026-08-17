9a7a0b75eb272a45459f57d9cd1148d07c0a879c
mahichate17-prog <mahichate17@gmail.com>
2026-08-17 14:44:33 +0530

    Update EmergencyModeScreen.kt

--- EmergencyModeScreen.kt
+++ EmergencyModeScreen.kt
@@ -55,6 +55,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.background
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
